748f2df9365165278ae9f28173134859182672d9
Tor <torgeirh@hotmail.com>
2018-01-30 21:31:31 -0500

    remove unused deps

--- systemjs-config.js
+++ systemjs-config.js
@@ -12,13 +12,9 @@
         '@angular/compiler': 'node_modules/@angular/compiler/bundles/compiler.umd.js',
         '@angular/platform-browser': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
         '@angular/platform-browser/animations': 'node_modules/@angular/platform-browser/bundles/platform-browser-animations.umd.js',
-        '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
         '@angular/http': 'node_modules/@angular/http/bundles/http.umd.js',
         '@angular/router': 'node_modules/@angular/router/bundles/router.umd.js',
-        '@angular/router/upgrade': 'node_modules/@angular/router/bundles/router-upgrade.umd.js',
         '@angular/forms': 'node_modules/@angular/forms/bundles/forms.umd.js',
-        '@angular/upgrade': 'node_modules/@angular/upgrade/bundles/upgrade.umd.js',
-        '@angular/upgrade/static': 'node_modules/@angular/upgrade/bundles/upgrade-static.umd.js',
       }
     });
   })(this);
