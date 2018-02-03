630e6532c0b0f8a9a26a8b9d04cfbe7300215488
Tor <torgeirh@hotmail.com>
2018-02-02 21:30:41 -0500

    angular otimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-
+const PurifyPlugin = require('@angular-devkit/build-optimizer').PurifyPlugin;
 const es2015Root = `${__dirname}/bazel-out/host/bin/src/apps/demo/prod_source.es6/angular_samples/src/apps/demo`;
 const output = process.argv[5];
 
@@ -9,12 +9,26 @@ module.exports = {
     filename: output
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        //TODO resolve this from Bazel node_modules
+        loader: `${__dirname}/node_modules/@angular-devkit/build-optimizer/webpack-loader`,
+        options: {
+          sourceMap: false
+        }
+      } 
+    ]
+  },
+
   resolve: {
     alias: {
       'angular_samples/src/apps/demo': `${es2015Root}`
     }
   },
   plugins: [
-    new webpack.optimize.ModuleConcatenationPlugin()
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new PurifyPlugin()
   ]
 }
