767f73720000dfb3159cb521aed873736735f2df
Torgeir Helgevold <torgeirh@hotmail.com>
2018-02-04 11:04:28 -0500

    formating

--- src/apps/demo/shared-components/tree-view/tree-view-demo.ts
+++ src/apps/demo/shared-components/tree-view/tree-view-demo.ts
@@ -1,27 +1,27 @@
 import {Component} from '@angular/core';
+
 import {TreeView} from './tree-view';
 import {Directory} from './directory';
 
 @Component({
-    template: '<h1>Recursive TreeView</h1><tree-view [directories]="directories"></tree-view>' +
-    '          <h4><a href="http://www.syntaxsuccess.com/viewarticle/recursive-treeview-in-angular-2.0">Read more here</a></h4>'
+  template: `<h1>Recursive TreeView</h1><tree-view [directories]="directories"></tree-view>
+             <h4><a href="http://www.syntaxsuccess.com/viewarticle/recursive-treeview-in-angular-2.0">Read more here</a></h4>`
 })
 
 export class TreeViewDemo {
-    directories: Array<Directory>;
+  directories: Array<Directory>;
 
-    constructor(){
-        this.loadDirectories();
-    }
+  constructor() {
+    this.loadDirectories();
+  }
 
-    loadDirectories(){
+  loadDirectories(){
+    const fall2014 = new Directory('Fall 2014',[],['image1.jpg','image2.jpg','image3.jpg']);
+    const summer2014 = new Directory('Summer 2014',[],['image10.jpg','image20.jpg','image30.jpg']);
+    const pics = new Directory('Pictures',[summer2014,fall2014],[]);
 
-        const fall2014 = new Directory('Fall 2014',[],['image1.jpg','image2.jpg','image3.jpg']);
-        const summer2014 = new Directory('Summer 2014',[],['image10.jpg','image20.jpg','image30.jpg']);
-        const pics = new Directory('Pictures',[summer2014,fall2014],[]);
+    const music = new Directory('Music',[],['song1.mp3','song2.mp3']);
 
-        const music = new Directory('Music',[],['song1.mp3','song2.mp3']);
-
-        this.directories = [pics,music];
-    }
+    this.directories = [pics,music];
+  }
 }
